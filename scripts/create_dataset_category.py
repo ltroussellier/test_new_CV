b9cef1d0078c511f7d5f9661d8e8e2e4f10b1170
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2025-06-16 10:22:04 +0200

    fix : rebuild dataset_category terms

--- scripts/create_dataset_category.py
+++ scripts/create_dataset_category.py
@@ -47,7 +47,7 @@ def convert() -> None:
                 "@context": "000_context.jsonld",
                 "id": term_id,
                 "type": "realm",
-                "drs_name": term_id,  # for esgvoc
+                "drs_name": category,  # for esgvoc
             }
 
             # Write term file
